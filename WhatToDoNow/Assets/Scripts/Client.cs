f2dd84480597b2f1d4e7f59dd5a9e2b37532fb00
vaalfreja <vaalfreja@gmail.com>
2015-01-24 17:44:55 +0300

    Button for send 1

--- WhatToDoNow/Assets/Scripts/Client.cs
+++ WhatToDoNow/Assets/Scripts/Client.cs
@@ -13,19 +13,28 @@ public class Client : MonoBehaviour {
 
     public Thread mThread;
 
+    public void ThreadSend(string message)
+    {
+        mRunning = true;
+        mThread = new Thread(() => Send(message));
+        mThread.Start();
+        print("Thread done...");
+    }
+
 	// Use this for initialization
     void Awake()
     {
-        mRunning = true;
-        ThreadStart ts = new ThreadStart(Send);
-        mThread = new Thread(ts);
-        mThread.Start();
-        print("Thread done...");
+        ThreadSend("This is a test");
 	}
 
-    void Send()
+    public void BtnSend()
     {
-        IPAddress ipAddress = IPAddress.Parse("10.10.8.38");
+        ThreadSend("1");
+    }
+
+    void Send(string message)
+    {
+        IPAddress ipAddress = IPAddress.Parse("10.10.8.39");
         IPEndPoint remoteEP = new IPEndPoint(ipAddress,1234);
 
         Socket sender = new Socket(AddressFamily.InterNetwork, 
@@ -38,27 +47,27 @@ public class Client : MonoBehaviour {
                 sender.RemoteEndPoint.ToString()));
 
             // Encode the data string into a byte array.
-            byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+            byte[] msg = Encoding.ASCII.GetBytes(message + "<EOF>");
 
             // Send the data through the socket.
             int bytesSent = sender.Send(msg);
 
-            byte[] bytes = new byte[10000];
+            //byte[] bytes = new byte[10000];
             // Receive the response from the remote device.
-            int bytesRec = sender.Receive(bytes);
-            Debug.Log(String.Format("Echoed test = {0}",
-                Encoding.ASCII.GetString(bytes,0,bytesRec)));
+            //int bytesRec = sender.Receive(bytes);
+            //Debug.Log(String.Format("Echoed test = {0}",
+            //    Encoding.ASCII.GetString(bytes,0,bytesRec)));
 
             // Release the socket.
             sender.Shutdown(SocketShutdown.Both);
             sender.Close();
                 
         } catch (ArgumentNullException ane) {
-            Debug.Log(String.Format("ArgumentNullException : {0}",ane.ToString()));
+            Debug.Log(String.Format("ArgumentNullException : {0}",ane.Message));
         } catch (SocketException se) {
-            Debug.Log(String.Format("SocketException : {0}",se.ToString()));
+            Debug.Log(String.Format("SocketException : {0}",se.Message));
         } catch (Exception e) {
-            Debug.Log(String.Format("Unexpected exception : {0}", e.ToString()));
+            Debug.Log(String.Format("Unexpected exception : {0}", e.Message));
         }
     }
 	
