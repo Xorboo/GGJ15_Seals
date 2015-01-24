fc793fa1782193f7bfb3e0547bbfd271569a28c7
Xorboo <xorboo@gmail.com>
2015-01-24 23:22:46 +0300

    Basic game field with animators

---
+++ WhatToDoNow/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    public float moveUpdateTime;
+    Vector3 Velocity = new Vector3();
+	// Use this for initialization
+	void Start() {
+        StartCoroutine("MoveCoroutine");
+	}
+
+    void Update()
+    {
+        //Debug.Log(rigidbody.velocity);
+    }
+
+	// Update is called once per frame
+	void FixedUpdate() {
+        //transform.position.Set(pos.x, pos.y, pos.z);
+        rigidbody.velocity = Velocity;
+	}
+
+    IEnumerator MoveCoroutine()
+    {
+        while (true)
+        {
+            UpdateVelocityKeyboard();
+            yield return new WaitForSeconds(moveUpdateTime);
+        }
+    }
+
+    void UpdateVelocity()
+    {
+        // TODO
+    }
+
+    // Temp function for 1 user control
+    void UpdateVelocityKeyboard()
+    {
+        Velocity.Set(0, 0, 0);
+        if (Input.GetKey(KeyCode.S))
+            Velocity.z -= speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.W))
+            Velocity.z += speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+            Velocity.x -= speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D))
+            Velocity.x += speed * Time.deltaTime;
+    }
+}
---
+++ WhatToDoNow/Assets/Scripts/UnitAnimator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitAnimator : MonoBehaviour
+{
+    Animator animator;
+    bool isFacingRight = true;
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        float dirSpeed = rigidbody.velocity.x;
+        animator.SetFloat("Speed", Mathf.Max(Mathf.Abs(dirSpeed), Mathf.Abs(rigidbody.velocity.z)));
+        if (dirSpeed > 0 && !isFacingRight ||
+            dirSpeed < 0 && isFacingRight)
+            Flip();
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
