ada2b5c7677c66cee36c26a8e4f0f48d434da2db
vaalfreja <vaalfreja@gmail.com>
2015-01-24 15:16:39 +0300

    Improving server and stuff

--- WhatToDoNow/Assets/Scripts/Serv.cs
+++ WhatToDoNow/Assets/Scripts/Serv.cs
@@ -5,9 +5,13 @@ using System.Net.Sockets;
 using System;
 using System.Text;
 using System.Threading;
+using System.Collections.Generic;
 
 public class Serv : MonoBehaviour
 {
+    public List<Color> playerColors;
+
+    private List<Socket> clients = new List<Socket>();
     private bool mRunning;
     public static string msg = "";
 
@@ -20,7 +24,7 @@ public class Serv : MonoBehaviour
         ThreadStart ts = new ThreadStart(Receive);
         mThread = new Thread(ts);
         mThread.Start();
-        print("Thread done...");
+        Debug.Log("Thread done...");
     }
 
     public void stopListening()
@@ -32,22 +36,21 @@ public class Serv : MonoBehaviour
     {
         tcp_Listener = new TcpListener(IPAddress.Any, 1234);
         tcp_Listener.Start();
-        print("Server Start");
+        Debug.Log("Server Start");
         while (mRunning)
         {
             // check if new connections are pending, if not, be nice and sleep 100ms
-            if (!tcp_Listener.Pending())
+            if (tcp_Listener.Pending())
             {
-                Thread.Sleep(100);
-            }
-            else
-            {
-                Socket ss = tcp_Listener.AcceptSocket();
+                Socket s = tcp_Listener.AcceptSocket();
+                clients.Add(s);
                 byte[] tempbuffer = new byte[10000];
-                ss.Receive(tempbuffer); // received byte array from client
+                s.Receive(tempbuffer); // received byte array from client
                 string strResult = Encoding.ASCII.GetString(tempbuffer);
-                print(strResult);
+                Debug.Log("Recieved: " + strResult);
+                SendColor(s, clients.Count - 1);
             }
+            Thread.Sleep(100);
         }
     }
 
@@ -60,5 +63,18 @@ public class Serv : MonoBehaviour
     { // stop listening thread
         stopListening(); // wait fpr listening thread to terminate (max. 500ms)
         mThread.Join(500);
+    }
+
+    void SendColor(Socket s, int number)
+    {
+        Debug.Log("Sending...");
+        Color color = number < playerColors.Count ? playerColors[number] : Utils.RandomColor();
+        SendText(s, "COLOR " + color.r + " " + color.g + " " + color.b);
+        Debug.Log("Sent!");
+    }
+
+    void SendText(Socket s, string text)
+    {
+        s.Send(Encoding.ASCII.GetBytes(text));
     }
 }
---
+++ WhatToDoNow/Assets/Scripts/Utils.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Utils
+{
+    public static Color RandomColor()
+    {
+        return Color.red;
+    }
+}
